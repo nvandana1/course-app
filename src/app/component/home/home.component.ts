4f32c2daa8f2b5ba9c88f93e48c5701226f36520
Vandana Nayak <nvandana1@gmail.com>
2024-02-22 21:11:55 +0530

    styling

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import data from 'src/assets/data.json';
 
 @Component({
   selector: 'app-home',
-  standalone:true,
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [CommonModule],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  banner = 'Discover Latest Courses on Angular';
+  data!: {
+    courseName: string;
+    author: string;
+    actualPrice: string;
+    discountPercentage: string;
+    tags: string[];
+    cart?:boolean;
+    wishlist?:boolean
+  }[];
+  checked: boolean = true;
 
+  ngOnInit(): void {
+    this.data = data;
+  }
 }
